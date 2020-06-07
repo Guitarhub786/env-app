2bdb8ffcda13eeeae50cb37b2d1b0b39841f5b5f
Guitarhub786 <codewithdrawal@gmail.com>
2020-06-07 05:15:52 +0100

    init commit for .env

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+
+console.log("Hi!")
+console.log("Your API_KEY is: ", process.env.API_KEY)
+console.log(process.env.PROCESSOR_ARCHITECTURE)
+console.log(process.env.PROCESSOR_IDENTIFIER)
+console.log(process.env.USERDOMAIN)
+console.log(process.env.USERNAME)
+
